d6c9bd2d406facaadcf60d8e7930f2f72cd91c76
SEBASTIAO DE OLIVEIRA MELO <54176273+sebastiao-oliveira@users.noreply.github.com>
2025-04-26 11:09:40 -0300

    correçao footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,26 +10,28 @@ const Footer = () => {
             <div className={styles.contentFooter}>
                 <div className={styles.copy}>
                     <img src={logo} alt='logo-footer'/>
-                    <p>Copyright &reg; - Todos os direitos reservados ao SAJU.</p>
+                    <div className={styles.contacts}>
+                        <div>
+                            <FaPhone/> 
+                            <span>(71) 3283-9050 </span>
+                        </div>
+                        <div>
+                            <MdEmail/> 
+                            <span>sajubahia@gmail.com</span>
+                        </div>
+                        <div id={styles.divLocation}>
+                            <RiMapPin2Fill/> 
+                            <span>R. da Paz, s/n, Térreo da Faculdade de Direito da UFBA, Graça Salvador, Bahia, Brasil. </span>
+                        </div>
+                    </div>
+                    <p>&copy; {new Date().getFullYear()} - Todos os direitos reservados ao SAJU.</p>
                 </div>
-                <nav className={styles.socials}>
-                    <FaBloggerB/>
-                    <FaFacebook/>
-                    <a href='https://www.instagram.com/sajubahia/' target='_blank' rel='noreferrer'><RiInstagramFill/></a>
-                </nav>
-                <div className={styles.contacts}>
-                    <div>
-                        <FaPhone/> 
-                        <span>(71) 3283-9050 </span>
-                    </div>
-                    <div>
-                        <MdEmail/> 
-                        <span>sajubahia@gmail.com</span>
-                    </div>
-                    <div id={styles.divLocation}>
-                        <RiMapPin2Fill/> 
-                        <span>R. da Paz, s/n, Térreo da Faculdade de Direito da UFBA, Graça Salvador, Bahia, Brasil. </span>
-                    </div>
+                <div className={styles.socialAndContacts}>
+                    <nav className={styles.socials}>
+                        <a href='https://blog.saju.com' target='_blank' rel='noreferrer'><FaBloggerB/></a>
+                        <a href='https://facebook.com/sajubahia' target='_blank' rel='noreferrer'><FaFacebook/></a>
+                        <a href='https://www.instagram.com/sajubahia/' target='_blank' rel='noreferrer'><RiInstagramFill/></a>
+                    </nav>
                 </div>
             </div>
         </footer>
